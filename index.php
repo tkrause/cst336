5e5ccddf77cfd32af9bc15aaec7c36fd4e5f2e1c
Thomas Krause <trueswordmaster@gmail.com>
2018-10-30 18:15:42 -0700

    Finished Lab 2

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <h2>Labs</h2>
 
     <a href="lab1/index.html">Lab 1 - Personal Portfolio</a><br>
+    <a href="lab2/index.php">Lab 2 - Slots</a><br>
 
     <h2>Homework</h2>
     <a href="hw1/index.html">Homework 1</a><br>
