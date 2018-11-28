a54a6b492cce91e3df5801e1e62f9675ba514449
Thomas Krause <trueswordmaster@gmail.com>
2018-11-27 22:08:03 -0800

    update front page listing

--- index.php
+++ index.php
@@ -8,11 +8,13 @@
     <a href="lab3/index.php">Lab 3 - Image Carousel</a><br>
     <a href="scart/index.php">Lab 4 - Shopping Cart</a><br>
     <a href="lab5/">Lab 5 - Product Search</a><br>
+    <a href="lab6/">Lab 6 - Admin Panel</a><br>
+    <a href="lab7/">Lab 7 - Hangman Game</a><br>
 
     <h2>Homework</h2>
     <a href="hw1/index.html">Homework 1</a><br>
     <a href="hw2/">Homework 2</a><br>
     <a href="hw3/">Homework 3</a><br>
-
+    <a href="hw4/">Homework 4 - Space Invaders</a><br>
     </body>
 </html>
